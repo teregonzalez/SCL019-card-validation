1ab23157a8993ac47d2721d38cc04bcc8d685e8d
teregonzalez <teregonzalezpro@gmail.com>
2021-12-02 13:09:15 -0300

    Se hacne cambios en el html y css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import validator from './validator.js';
+//import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+
+
+const arr = [3, 2, 1];
+console.log(arr.slice(0)); // => [3, 2, 1]
+console.log(arr.slice(1)); // => [2, 1]
+console.log(arr.slice(2)); // => [1]
+console.log(arr.slice(-1)); // => [3, 2]
+console.log(arr.slice(0, 1)); // => [3]
+console.log(arr); // => [3, 2, 1];
