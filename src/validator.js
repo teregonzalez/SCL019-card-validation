ad2e6706184c32b610e9acf04f877542f7378319
teregonzalez <teregonzalezpro@gmail.com>
2021-12-13 10:02:39 -0300

    Se cambia formato de fecha en html

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,10 @@
-const validator = {
-  // ...
-};
+const validator = { //Se crea objeto validator
 
-export default validator;
+  isValid(creditCardNumber){
+    if(creditCardNumber.length < 1) return false;// no permite que este vacio
+    let numberArray = creditCardNumber.split(''); //Convertimos el numero de la tarjeta a un array
+   console.log(numberArray); //Mostramos el array que se creo del numero de tarjeta
+  //let 
+  }
+  }//
+  export default validator;
