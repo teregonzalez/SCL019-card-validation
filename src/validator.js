5c29f067464f1563abb1c89cba1525ba13224957
Aymara <aymara.g.e@gmail.com>
2021-12-17 14:46:25 -0300

    Co-authored-by: Tere Gonzalez <teregonzalez@users.noreply.github.com>

--- src/validator.js
+++ src/validator.js
@@ -1,25 +1,49 @@
 const validator = { //Se crea objeto validator
   
   isValid(creditCardNumber){
-     let numberArray = creditCardNumber.split(''); //Convertimos el numero de la tarjeta a un array
-     console.log(numberArray);//Mostramos el array que se creo del numero de tarjeta
+   
+     let numberArray = creditCardNumber.split('');
+    
+     for(let i= 0; i < numberArray.length; i++){ 
+       if (i % 2 === 0) {
+           numberArray[i] = numberArray[i] * 2;
+           if ( numberArray[i] >= 10) { 
+            numberArray[i] = String(numberArray[i])
+         .split("")
+          .map(Number)
+           .reduce(function (a, b){
+          return a + b
+        });
+      }
+    }
+    else{
+      numberArray[i] = numberArray[i] * 1;
+  }
+     }
+  let valid = numberArray.reduce(function (a, b) {
+    return a + b;
+    }); 
+    if (valid % 10 == 0) {
+      //tarjeta valida
+      console.log("es valido");
+      return true;
+    } else {
+      //tarjeta invalida
+      console.log("No es valido");
+      return false;
+    }
 
-   let positionPar = 0;
-   for(let i= 0; i < numberArray.length; i++){
-     let positionPar= i + 2;
-     if(positionPar % 2 === 0){
-       let numMultiplicar = numberArray[i] * 2;
-       console.log(numMultiplicar)}
-  } 
 }
+  };
+    
+           /* 
+      
 }
-export default validator;
-//numberArray[i];
-//console.log(listArr);
-//let positionPar = i;
-//console.log(positionPar);
-  
+} 
   //maskify(creditCardNumber) {
-    //return creditCardNumber.slice(0,-4).replace(/./g, "#") + creditCardNumber.slice(-4);
+  //return creditCardNumber.slice(0,-4).replace(/./g, "#") + creditCardNumber.slice(-4);
       //reemplaza los numeros de la tarjeta desde la posicion 0 a la -4 por #
       //Y solo muestra los ultimos cuatro numeros
+   */   
+
+export default validator;
