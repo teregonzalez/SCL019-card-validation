052b4b511e0ea4f83055d9a56e06c075bfb64882
Aymara <aymara.g.e@gmail.com>
2021-12-18 11:35:57 -0300

    avance de js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', () => {
   const creditCardNumber = document.getElementById('cNumber').value;
    //console.log(creditCardNumber);
+  
 
-   validator.isValid(creditCardNumber);
-   //validator.maskify(creditCardNumber);
+   let validacion = validator.isValid(creditCardNumber);
+   if (validacion === true){
+   document.getElementById("message").innerHTML="Su número de tarjeta es Válida";
+  }
+   else { 
+    document.getElementById("message").innerHTML="Su número de tarjeta NO es Válida";
+   };
+
+
+   let enmascarar = validator.maskify(creditCardNumber);
+ document.getElementById("cNumber").value = enmascarar;
+
 });
