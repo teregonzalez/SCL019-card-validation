0133de9e8754f047e8dc87c436c3cd6b64cb0b5e
teregonzalez <teregonzalezpro@gmail.com>
2021-12-20 12:00:49 -0300

    Se actualiza el validator

--- src/validator.js
+++ src/validator.js
@@ -3,10 +3,10 @@ const validator = { //Se crea objeto validator
 
   isValid(creditCardNumber){
  
-   let numberArray = creditCardNumber.split('').map(Number);
+   let numberArray = creditCardNumber.split('').map(Number).reverse;
   
    for(let i= 0; i < numberArray.length; i++){ 
-     if (i % 2 === 0) {
+     if (i % 2 != 0) {
          numberArray[i] = numberArray[i] * 2;
          if ( numberArray[i] >= 10) { 
           numberArray[i] = String(numberArray[i])
@@ -23,7 +23,7 @@ const validator = { //Se crea objeto validator
       console.log(numberArray)
       let valid = 0
       for(let i= 0; i<numberArray.length; i++){
-        valid+=numberArray[i];
+        valid+=numberArray[i]
       }
       //console.log(valid)
       if (valid % 10 == 0) {
